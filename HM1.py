2231b961c8ce5f134d331083ffdd2a4405bb812d
Dima Morozov <morozov.jangool@gmail.com>
2024-03-18 20:57:44 +0200

    Git 18 03

---
+++ HM1.py
@@ -0,0 +1,5 @@
+sp = [1,2,3,4,5]
+summa = 0
+for i in sp:
+    summa += i
+print(summa)
---
+++ HM2.py
@@ -0,0 +1,7 @@
+sp = [1,2,3,4,5]
+i = 0
+summa = 0
+while i < len(sp):
+    summa += sp[i]
+    i += 1
+print(summa)
